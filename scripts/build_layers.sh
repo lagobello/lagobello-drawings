85b3705956f128ecb67f703545619bf785ea1ff5
Vittorio Alfieri <vittorio88@gmail.com>
2025-07-09 16:05:55 -0500

    Add newline at end of styles.kml

--- scripts/build_layers.sh
+++ scripts/build_layers.sh
@@ -83,9 +83,10 @@ export_layer () {        # $1=SQL where  $2=stem  (no ext)
       *) STYLE_NAME="defaultStyle" ;;
     esac
 
-    # Add NetworkLink to styles.kml and replace inline styles with styleUrl
-    sed -e 's|<Document id="root_doc">|<Document id="root_doc">\n  <NetworkLink><Link><href>https://lagobello.github.io/lagobello-drawings/web/styles.kml</href></Link></NetworkLink>|' \
-        -e 's|<Style><LineStyle><color>ff0000ff</color></LineStyle><PolyStyle><fill>0</fill></PolyStyle></Style>|<styleUrl>#'${STYLE_NAME}'</styleUrl>|g' \
+    # Replace default inline style with reference to shared style document
+    # using an absolute URL that includes the style ID.
+    sed -e 's|<Style><LineStyle><color>ff0000ff</color></LineStyle><PolyStyle><fill>0</fill></PolyStyle></Style>|<styleUrl>https://lagobello.github.io/lagobello-drawings/web/styles.kml#'${STYLE_NAME}'</styleUrl>|g' \
+        -e '/<NetworkLink><Link><href>https:\/\/lagobello.github.io\/lagobello-drawings\/web\/styles.kml<\/href><\/Link><\/NetworkLink>/d' \
         "$KML_TMP" > "$KML"
     rm "$KML_TMP"
 
